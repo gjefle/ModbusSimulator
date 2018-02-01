db1a1ffe0f6686be70ce3c4388289aa02ef63f0d
Gjermund Fleten <gjermufl@gmail.com>
2018-02-01 20:26:32 +0100

    Initial commit

---
+++ ModbusSimulator/ModbusNodes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ModbusSimulator
+{
+    public partial class ModbusSimulator : Form
+    {
+        public ModbusSimulator()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ModbusSimulator/Node.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Modbus.Data;
+using Modbus.Device;
+
+namespace ModbusSimulator
+{
+    public class Node : IDisposable
+    {
+        private readonly Thread _thread;
+        protected ModbusSlave SlaveNode;
+        private TcpListener _listener;
+        private IPAddress _ip;
+        private TaskAwaiter modbusNodeTask;
+
+        //private readonly Semaphore _semaphore;
+        public Node(string name, IPAddress ip)
+        {
+            //_semaphore = semaphore;
+            _ip = ip;
+            _thread = new Thread(Runner) { Name = name };
+            _listener = new TcpListener(_ip, 502);
+            _listener.Start();
+            SlaveNode = ModbusTcpSlave.CreateTcp(0, _listener);
+            SlaveNode.DataStore = DataStoreFactory.CreateDefaultDataStore();
+            _thread.Start();
+        }
+
+        public ushort GetCommands(int start) => SlaveNode.DataStore.HoldingRegisters[start];
+        private void Runner()
+        {
+            modbusNodeTask = SlaveNode.ListenAsync().GetAwaiter();
+            try
+            {
+                modbusNodeTask.GetResult();
+            }
+            catch (TaskCanceledException te) { }
+        }
+
+        public void Dispose()
+        {
+            _listener.Stop();
+            _thread.Abort();
+        }
+    }
+}
